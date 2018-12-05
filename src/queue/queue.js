a7bba6b1f72135c4652497c9d5b3e242394f65f2
Deepak Jayaram <dpk@Deepaks-MacBook-Pro.local>
2018-12-05 00:17:46 -0500

    a better queue implementation

--- src/queue/queue.js
+++ src/queue/queue.js
@@ -1,18 +1,22 @@
-// First in, first out
-
+// First in, First out
 class Queue {
     constructor(...arr) {
         this.arr = arr;
+    }
+    get length() {
+        return this.arr.length;
     }
     enqueue(el) {
         return el ? this.arr.unshift(el) : this.arr;
     }
     dequeue() {
-        const el = this.arr.length ? this.arr.pop() : [];
-        return el;
+        return !this.isEmpty() ? this.arr.pop() : [];
     }
     peek() {
-        return this.arr[this.arr.length - 1];
+        return !this.isEmpty() ? this.arr[this.length - 1] : null;
+    }
+    isEmpty() {
+        return this.length === 0;
     }
     print() {
         return this.arr;
--- src/queue/queue.test.js
+++ src/queue/queue.test.js
@@ -3,7 +3,7 @@ const Queue = require('./queue');
 describe('A queue', () => {
     it('when Initialized, peeking at it, results undefined', () => {
         const queue = new Queue();
-        expect(queue.peek()).toEqual(undefined);
+        expect(queue.peek()).toEqual(null);
     });
 
     it('when Initialized with items, peeking at it yields the last item', () => {
@@ -37,5 +37,31 @@ describe('A queue', () => {
     it('dequeuing a queue returns us the element that was dequeued', () => {
         const queue = new Queue(1, 2, 3);
         expect(queue.dequeue()).toEqual(3);
+    });
+
+    it('dequeuing a queue more than required, returns an empty queue', () => {
+        const queue = new Queue(1, 2);
+        queue.dequeue();
+        queue.dequeue();
+        queue.dequeue();
+        expect(queue.print()).toEqual([]);
+    });
+
+    it('isEmpty returns false when the queue is not empty', () => {
+        const queue = new Queue(1, 2, 3);
+        expect(queue.isEmpty()).toEqual(false);
+    });
+
+    it('isEmpty returns true when the queue is empty', () => {
+        const queue = new Queue();
+        expect(queue.isEmpty()).toEqual(true);
+    });
+
+    it('isEmpty returns true when all items in the queue are dequeued', () => {
+        const queue = new Queue(1, 2, 4);
+        queue.dequeue();
+        queue.dequeue();
+        queue.dequeue();
+        expect(queue.isEmpty()).toEqual(true);
     });
 });
